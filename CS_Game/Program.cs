8b92b96ddb679dda0e1be6fd2ee751ad8ded628d
Gabriel Souza de Aguiar Penke <gabriel.penke@gruposn.com.br>
2025-06-12 21:11:20 -0300

    first commit

---
+++ CS_Game/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public class CS_Game
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Bem vindo(a) ao CS Game!");
+
+    }
+}
---
+++ CS_Game/Time.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Security.Principal;
+
+public class Time
+{
+	public string nome;
+	public string pais;
+	public Time()
+	{
+	}
+}
