0998b49648de848680efa5096ef1e4576cabf16b
Gabriel Souza de Aguiar Penke <gabriel.penke@gruposn.com.br>
2025-06-13 21:56:47 -0300

    create players in team class

--- CS_Game/Team.cs
+++ CS_Game/Team.cs
@@ -5,5 +5,10 @@ public class Team
 {
 	public string name;
 	public string country;
-	
+	public string player_01;
+    public string player_02;
+    public string player_03;
+    public string player_04;
+    public string player_05;
+
 }
